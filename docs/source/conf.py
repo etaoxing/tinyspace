61e4fed855e1a2e6f868262bf097a604bd289338
etaoxing <etaoxing@users.noreply.github.com>
2022-04-26 17:27:31 -0400

    Cleanup docs conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -41,10 +41,12 @@ import sys
 
 # -- Project information -----------------------------------------------------
 
-project = "tinyspace"
-copyright = f"{datetime.date.today().year}, {project} contributors"
-author = "etaoxing"
-repo_url = "https://github.com/etaoxing/tinyspace"
+sys.path.insert(0, os.path.abspath("../../"))
+from setup import NAME, AUTHOR, URL
+
+project = NAME  # change this if repo name is different than module folder name
+copyright = f"{datetime.date.today().year}, {AUTHOR}"
+repo_url = URL
 
 # -- Path setup --------------------------------------------------------------
 
@@ -52,7 +54,6 @@ repo_url = "https://github.com/etaoxing/tinyspace"
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 sys.path.insert(0, os.path.join(os.path.abspath("../../"), project))
-# from version import tag as version, version as release
 from version import commit
 
 # -- General configuration ---------------------------------------------------
@@ -112,7 +113,7 @@ exclude_patterns = ["build", "Thumbs.db", ".DS_Store"]
 source_suffix = [".rst", ".md"]
 
 # Configure viewcode extension.
-code_url = f"{repo_url}/blob/{commit}"
+code_url = f"{URL}/blob/{commit}"
 
 def linkcode_resolve(domain, info):
     # Non-linkable objects from the starter kit in the tutorial.
@@ -153,7 +154,6 @@ def linkcode_resolve(domain, info):
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 html_theme = "furo"
-
 html_theme_options = {
     "light_css_variables": {
         "color-brand-primary": "#306998",  # blue from logo
@@ -167,7 +167,7 @@ html_theme_options = {
     "footer_icons": [
         {
             "name": "GitHub",
-            "url": repo_url,
+            "url": URL,
             "html": """
                 <svg stroke="currentColor" fill="currentColor" stroke-width="0" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M8 0C3.58 0 0 3.58 0 8c0 3.54 2.29 6.53 5.47 7.59.4.07.55-.17.55-.38 0-.19-.01-.82-.01-1.49-2.01.37-2.53-.49-2.69-.94-.09-.23-.48-.94-.82-1.13-.28-.15-.68-.52-.01-.53.63-.01 1.08.58 1.23.82.72 1.21 1.87.87 2.33.66.07-.52.28-.87.51-1.07-1.78-.2-3.64-.89-3.64-3.95 0-.87.31-1.59.82-2.15-.08-.2-.36-1.02.08-2.12 0 0 .67-.21 2.2.82.64-.18 1.32-.27 2-.27.68 0 1.36.09 2 .27 1.53-1.04 2.2-.82 2.2-.82.44 1.1.16 1.92.08 2.12.51.56.82 1.27.82 2.15 0 3.07-1.87 3.75-3.65 3.95.29.25.54.73.54 1.48 0 1.07-.01 1.93-.01 2.2 0 .21.15.46.55.38A8.013 8.013 0 0 0 16 8c0-4.42-3.58-8-8-8z"></path>
@@ -178,8 +178,7 @@ html_theme_options = {
     ],
 }
 
-html_logo = "_static/logo.svg"
-
+# html_logo = "_static/logo.svg"
 # html_favicon = "_static/favicon.ico"
 
 # Add any paths that contain custom static files (such as style sheets) here,
