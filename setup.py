13701895cb86eb337b6d1fa9ad6ae4e5d0e0f2e7
etaoxing <etaoxing@users.noreply.github.com>
2022-05-09 11:29:18 -0400

    Exclude tests package [skip ci]

--- setup.py
+++ setup.py
@@ -90,7 +90,7 @@ if __name__ == "__main__":
         url=URL,
         # download_url=f'{URL}/archive/{__version__}.tar.gz',
         license="MIT",
-        packages=find_packages(),
+        packages=find_packages(exclude=["tests*"]),
         include_package_data=True,
         install_requires=install_requires,
         extras_require=extras_deps,
